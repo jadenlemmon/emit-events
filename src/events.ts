64e8ec5a73f49ae1ec832f810308d47ac261c2b4
jlemm45 <jadenlemmon@gmail.com>
2019-02-28 07:38:28 -0600

    repo.init()

---
+++ src/events.ts
@@ -0,0 +1,18 @@
+type Callback = (payload: Object) => void;
+
+class Events {
+  storedEvents: any = {};
+
+  emit = (event: string, payload: Object) => {
+    const storedEvent = this.storedEvents[event];
+    if (storedEvent && typeof storedEvent === "function") {
+      storedEvent(payload);
+    }
+  };
+
+  subscribe(event: string, callback: Callback) {
+    this.storedEvents[event] = callback;
+  }
+}
+
+export default new Events();
